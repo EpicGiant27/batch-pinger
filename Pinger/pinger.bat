b94ee6c13cb8ec4cd6a35fc2779aabc963b9a5aa
EpicGiant27 <82335462+EpicGiant27@users.noreply.github.com>
2021-05-16 13:18:30 +0100

    Add files via upload

--- Pinger/pinger.bat
+++ Pinger/pinger.bat
@@ -1,7 +1,24 @@
 @echo off
-color 09
+chcp 65001
+cls
+:: goes to the "Texts" directory
+cd Texts
+::tell the user to set color
+type welcome.txt
+pause
+cls
+::shows the user color options
+type colorlist.txt
+set /p color=
+pause
+::changes the color
+color %color%
+cls
 type title.txt
-echo .
+:: goes back to parent directory
+cd ..
 title Pinger
-set /p ip=Enter the IP Address you want to 
-ping %ip% -t
+set /p ip=Enter the IP Address you want to ping: 
+PING -t %ip% | FIND "TTL="
+IF ERRORLEVEL 1 echo Ip is offline.
+IF NOT ERRORLEVEL 1 echo Reply from %ip%.
